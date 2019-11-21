8c5f6fef186159ed7d56d33384b27c3030e817d8
Tomás Honório Oliveira <2190338@c2-lcm-m09.local>
2019-11-21 20:51:18 +0000

    Project Inicialization

---
+++ PocketPrescription/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PocketPrescription
+//
+//  Created by Tomás Honório Oliveira on 21/11/2019.
+//  Copyright © 2019 Tomás Honório Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
