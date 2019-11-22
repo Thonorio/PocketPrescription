0a402906b8c839bafaa817a4cec9461dc21825f7
Tomás Honório Oliveira <2190338@c2-lcm-m09.local>
2019-11-22 20:35:52 +0000

    Refactor the structure of the projecto to respect MVC

---
+++ PocketPrescription/Model/Medication.swift
@@ -0,0 +1,25 @@
+//
+//  Medication.swift
+//  PocketPrescription
+//
+//  Created by Tomás Honório Oliveira on 22/11/2019.
+//  Copyright © 2019 Tomás Honório Oliveira. All rights reserved.
+//
+
+import Foundation
+
+class Medication {
+
+    var name: String
+    var packageQuantity: UInt8
+    var category: String
+    var levelOfImportance: LevelOfImportance
+    
+    init() {
+        name = ""
+        packageQuantity = 0
+        category = ""
+        levelOfImportance = LevelOfImportance.normalImportance
+    }
+    
+}
