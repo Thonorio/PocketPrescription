43313141f05cbc136d5b97ff369a8025ec0c2a8c
Thonorio <tomas.honorio@gmail.com>
2019-12-23 03:02:48 +0000

    Refactor Table Lists Layout

---
+++ PocketPrescription/View/CustomViewCellSwift/MedicationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MedicationTableViewCell.swift
+//  PocketPrescription
+//
+//  Created by Tomas Honorio on 23/12/2019.
+//  Copyright © 2019 Tomás Honório Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class MedicationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
