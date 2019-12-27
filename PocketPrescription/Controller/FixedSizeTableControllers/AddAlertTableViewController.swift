3a942cef48e65bd30d1103bc2dd664d32d87ccc6
Thonorio <tomas.honorio@gmail.com>
2019-12-27 06:47:02 +0000

    04.3 Inputs change labels accordingly

--- PocketPrescription/Controller/FixedSizeTableControllers/AddAlertTableViewController.swift
+++ PocketPrescription/Controller/FixedSizeTableControllers/AddAlertTableViewController.swift
@@ -11,7 +11,6 @@ import CoreData
 import UserNotifications
 
 class AddAlertTableViewController: UITableViewController, UNUserNotificationCenterDelegate {
-
         
     //Outlets
     @IBOutlet weak var addAlertDatePicker: UIDatePicker!
@@ -25,6 +24,7 @@ class AddAlertTableViewController: UITableViewController, UNUserNotificationCent
     var selectedDate: Date!
     let ENTITIE: String = "Alert"
     var medications: [NSManagedObject] = []
+    var alertSubmit: UIAlertAction?
     
     // Core Data
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -33,74 +33,251 @@ class AddAlertTableViewController: UITableViewController, UNUserNotificationCent
     
     
     // MARK: - Life Cicle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loadData()
         // Inicialize Date
-        selectedDate = self.getDataFormated()
-        
+        selectedDate = self.addAlertDatePicker.date
+
         // Add Listener to the Date Picker
         addAlertDatePicker.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
-        
+
         // Configure User Notification Center
         UNUserNotificationCenter.current().delegate = self
-        
+
         // Limit Amount of Colls
         tableView.tableFooterView = UIView()
-    }    
+    }
     
-    // MARK: - Functionality
-    
-    @IBAction func okAddAlert(_ sender: Any) {
-        let newAlert = NSManagedObject(entity: entity!, insertInto: context)
-            
-        // Create Notification
-        self.createNotification()
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         
-        //Save info to Core Data
-        newAlert.setValue(addAlertLabel.text, forKey: "name")
-        for med in self.medications {
-            newAlert.setValue( NSSet(object: med), forKey: "medications")
-        }
-               
-        self.saveToCoreData()
-    }
-    
-    // Function executed by the listener
-    @objc func dateChanged (){
-        selectedDate = self.getDataFormated()
-    }
-    
-    // Translate UIDataPiker into  Date
-    func getDataFormated () -> Date {
-        // Format to usable format
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm"
         
-        // Return the Date
-        let dateAsString = dateFormatter.string(from: addAlertDatePicker.date)
-        return dateFormatter.date(from: dateAsString)!
+        
+        switch indexPath.row {
+            // Repeat
+            case 0:
+                // Create alert controller.
+                self.alertEditRepeat()
+            // Label
+            case 1:
+                self.alertEditLabel()
+            // Starts
+            case 2:
+                self.alertEditStartDate()
+            // Ends
+            case 3:
+                self.alertEditEndDate()
+            default:
+                return
+            }
     }
     
-    func alertEditLabel () {
-        // Create alert controller.
-        let alertController = UIAlertController(title: "Editing Label", message: nil, preferredStyle: .alert)
-
-        // COnfigure what to show inside it
-        alertController.addTextField { (textField) in
-            textField.text = "Add a Custom Label"
+     
+     // MARK: - Functionality
+     
+     @IBAction func okAddAlert(_ sender: Any) {
+         let newAlert = NSManagedObject(entity: entity!, insertInto: context)
+             
+         // Create Notification
+         self.createNotification()
+         
+         //Save info to Core Data
+         newAlert.setValue(addAlertLabel.text, forKey: "name")
+         for med in self.medications {
+             newAlert.setValue( NSSet(object: med), forKey: "medications")
+         }
+                
+         self.saveToCoreData()
+     }
+    
+    // When the text field dosn´t have anny text the Ok button is not available
+    @objc func textFieldDidChange(sender: UITextField){
+        if sender.text == "" {
+            alertSubmit!.isEnabled = false
+        }else{
+            alertSubmit!.isEnabled = true
         }
+    }
+    
+    // MARK: - Alert Functionalities
+    
+    // Todo Shpuld be hours not hours of the day
+    func alertEditRepeat ()  {
+        // Create and configure view controller
+        let viewController = UIViewController()
+        viewController.preferredContentSize = CGSize(width: 250,height: 250)
 
-        //  Grab the value from the text field
-        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alertController] (_) in
-            self.addAlertLabel.text = alertController?.textFields![0].text
+        // Create and add data picker to view controller
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        datePicker.datePickerMode = .time
+        viewController.view.addSubview(datePicker)
+
+        // Add options
+        let editRadiusAlert = UIAlertController(title: "Repeat", message: nil, preferredStyle: .alert)
+        editRadiusAlert.setValue(viewController, forKey: "contentViewController")
+        editRadiusAlert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak editRadiusAlert] (_) in
+            self.addAlertRepeatInterval.text = self.getDataFormatedAsString(datePicker.date, "HH:mm")
         }))
+           
+        editRadiusAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
 
+        // Present alert
+        self.present(editRadiusAlert, animated: true)
+    }
+    
+    func alertEditLabel ()  {
+        let alertController = UIAlertController(title: "Label this Alert", message: nil, preferredStyle: .alert)
+        
+        // Add Ok (Submit) Option and info destination
+        alertSubmit = UIAlertAction(title: "Done", style: .default, handler: { [weak alertController] (_) in
+            self.addAlertLabel.text = alertController?.textFields![0].text
+        })
+        
+        // Add Cancel option
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        // Ok button starts as not enabled
+        alertSubmit!.isEnabled = false
+        
+        // Add the text filed to the alert
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Add a Custom Label"
+            
+            // Add listener to text field (when empty)
+            textField.addTarget(self, action: #selector(AddAlertTableViewController.textFieldDidChange), for: UIControl.Event.editingChanged)
+        }
+        
+        // Grab the value from the text field
+        alertController.addAction(alertSubmit!)
+        alertController.addAction(cancelAction)
+        
         // Show allert
         self.present(alertController, animated: true, completion: nil)
     }
     
+    func alertEditStartDate ()  {
+        // Create and configure view controller
+        let viewController = UIViewController()
+        viewController.preferredContentSize = CGSize(width: 250,height: 250)
+        
+        // Create and add data picker to view controller
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        datePicker.datePickerMode = .date
+        viewController.view.addSubview(datePicker)
+        
+        // Add options
+        let editRadiusAlert = UIAlertController(title: "Starting Date", message: nil, preferredStyle: .alert)
+        editRadiusAlert.setValue(viewController, forKey: "contentViewController")
+        editRadiusAlert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak editRadiusAlert] (_) in
+            self.addAlertStartDate.text = self.getDataFormatedAsString(datePicker.date, "dd/MM/yyyy")
+        }))
+            
+        editRadiusAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        // Present alert
+        self.present(editRadiusAlert, animated: true)
+    }
+    
+    func alertEditEndDate () {
+        // Create and configure view controller
+        let viewController = UIViewController()
+        viewController.preferredContentSize = CGSize(width: 250,height: 250)
+
+        // Create and add data picker to view controller
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        datePicker.datePickerMode = .date
+        viewController.view.addSubview(datePicker)
+
+        // Add options
+        let editRadiusAlert = UIAlertController(title: "Ending Date", message: nil, preferredStyle: .alert)
+        editRadiusAlert.setValue(viewController, forKey: "contentViewController")
+        editRadiusAlert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak editRadiusAlert] (_) in
+            self.addAlertEndDate.text = self.getDataFormatedAsString(datePicker.date, "dd/MM/yyyy")
+        }))
+          
+        editRadiusAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        // Present alert
+        self.present(editRadiusAlert, animated: true)
+    }
+    
+    
+    // MARK: - Data Picker
+    
+    // Function executed by the listener
+    @objc func dateChanged (){
+        selectedDate = self.addAlertDatePicker.date
+    }
+    
+    // Translate UIDataPiker into  Date
+    func  getDataFormatedAsString(_ origianalDate: Date, _ format: String) -> String {
+        // Format to usable format
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        
+        // Return the Date
+        return dateFormatter.string(from: origianalDate)
+    }
+    
+    // MARK: - Notifications
+
+    private func createNotification(){
+        UNUserNotificationCenter.current().getNotificationSettings { (notificationSettings) in
+            switch notificationSettings.authorizationStatus {
+                case .notDetermined:
+                    self.requestAuthorization(completionHandler: { (success) in
+                    guard success else { return}
+
+                    // Schedule Local Notification
+                    self.scheduleLocalNotification()
+                    })
+                case .authorized:
+                    // Schedule Local Notification
+                    self.scheduleLocalNotification()
+                case .denied:
+                    print("Application Not Allowed to Display Notifications")
+                    default: break
+            }
+        }
+    }
+
+    // Notification Center
+    private func requestAuthorization(completionHandler: @escaping (_ success: Bool) -> ()) {
+        // Request Authorization
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
+            if let error = error {
+                print("Request Authorization Failed (\(error), \(error.localizedDescription))")
+            }
+            completionHandler(success)
+        }
+    }
+
+    private func scheduleLocalNotification() {
+        // Create Notification Content
+        let notificationContent = UNMutableNotificationContent()
+
+        // Configure Notification Content
+        notificationContent.title = addAlertLabel.text ?? "Something went Wrong"
+        notificationContent.body = "Remeber to take: "
+
+        // Add Trigger
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+
+        // Create Notification Request
+        let notificationRequest = UNNotificationRequest(identifier: "cocoacasts_local_notification", content: notificationContent, trigger: notificationTrigger)
+
+        // Add Request to User Notification Center
+        UNUserNotificationCenter.current().add(notificationRequest) { (error) in
+            if let error = error {
+                print("Unable to Add Notification Request (\(error), \(error.localizedDescription))")
+            }
+        }
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert])
+    }
     
     // MARK: - Core Data
     
@@ -123,7 +300,7 @@ class AddAlertTableViewController: UITableViewController, UNUserNotificationCent
             print("Failed")
         }
     }
-    
+       
     func saveToCoreData(){
        do {
           try context.save()
@@ -132,115 +309,34 @@ class AddAlertTableViewController: UITableViewController, UNUserNotificationCent
        }
     }
     
-    // MARK: - Notifications
-    
-    private func createNotification(){
-       UNUserNotificationCenter.current().getNotificationSettings { (notificationSettings) in
-           switch notificationSettings.authorizationStatus {
-               case .notDetermined:
-                   self.requestAuthorization(completionHandler: { (success) in
-                       guard success else { return}
-
-                       // Schedule Local Notification
-                       self.scheduleLocalNotification()
-                   })
-               
-               case .authorized:
-                   // Schedule Local Notification
-                   self.scheduleLocalNotification()
-               
-               case .denied:
-                   print("Application Not Allowed to Display Notifications")
-               default: break
-           }
-       }
-    }
-
-    // Notification Center
-    private func requestAuthorization(completionHandler: @escaping (_ success: Bool) -> ()) {
-       // Request Authorization
-       UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
-           if let error = error {
-               print("Request Authorization Failed (\(error), \(error.localizedDescription))")
-           }
-           completionHandler(success)
-       }
-    }
-
-    private func scheduleLocalNotification() {
-       // Create Notification Content
-       let notificationContent = UNMutableNotificationContent()
-       
-       // Configure Notification Content
-       notificationContent.title = addAlertLabel.text ?? "Something went Wrong"
-       notificationContent.body = "Remeber to take: "
-
-       // Add Trigger
-       let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
-
-       // Create Notification Request
-       let notificationRequest = UNNotificationRequest(identifier: "cocoacasts_local_notification", content: notificationContent, trigger: notificationTrigger)
-
-       // Add Request to User Notification Center
-       UNUserNotificationCenter.current().add(notificationRequest) { (error) in
-           if let error = error {
-               print("Unable to Add Notification Request (\(error), \(error.localizedDescription))")
-           }
-       }
-    }
-
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-       completionHandler([.alert])
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        switch indexPath.row {
-            // Repeat
-            case 0:
-                self.alertEditLabel()
-            // Label
-            case 1:
-                self.alertEditLabel()
-            // Starts
-            case 2:
-                self.alertEditLabel()
-            // Ends
-            case 3:
-                self.alertEditLabel()
-            default:
-                return
-            }
-    }
-
-    
     // MARK: - Interactions
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         
         if segue.destination is MedicationInclusionTableViewController
         {
-            print("sent")
             let vc = segue.destination as? MedicationInclusionTableViewController
             vc?.medications2 = self.medications
-        }
-
-        let addOrSubtractMoneyVC = segue.destination as! MedicationInclusionTableViewController
-        addOrSubtractMoneyVC.callback = { (medication, state) in
-            let currentMedicationText = self.addAlertMedicationList.text ?? ""
-            let medicationName = medication.value(forKey: "name") as? String  ?? ""
             
-            // Easier to read (Note: medication is an optional)
-            if state == true {
-                // Append to last spot
-                self.medications.append(medication)
-                self.addAlertMedicationList.text = currentMedicationText + " " + medicationName
-            }else{
-                // Remove filterd object
-                self.medications = self.medications.filter{ $0 != medication }
-                self.addAlertMedicationList.text = currentMedicationText.replacingOccurrences(of: medicationName, with: "")
-            }
+            let addOrSubtractMoneyVC = segue.destination as! MedicationInclusionTableViewController
+                   addOrSubtractMoneyVC.callback = { (medication, state) in
+                       let currentMedicationText = self.addAlertMedicationList.text ?? ""
+                       let medicationName = medication.value(forKey: "name") as? String  ?? ""
+                       
+                       // Easier to read (Note: medication is an optional)
+                       if state == true {
+                           // Append to last spot
+                           self.medications.append(medication)
+                           self.addAlertMedicationList.text = currentMedicationText + " " + medicationName
+                       }else{
+                           // Remove filterd object
+                           self.medications = self.medications.filter{ $0 != medication }
+                           self.addAlertMedicationList.text = currentMedicationText.replacingOccurrences(of: medicationName, with: "")
+                       }
+                   }
+            return
         }
+        
+        okAddAlert(sender as Any)
     }
 }
